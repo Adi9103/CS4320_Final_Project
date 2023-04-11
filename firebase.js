8c3150820969b3290d8ed0c20f133b7ab34b9b0c
Phillip Lei <77305686+phillipl21@users.noreply.github.com>
2023-04-11 15:05:30 -0500

    Fuck you

---
+++ firebase.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
+import { getDatabase, ref, child, get, set } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBhjZ1zYPBKw5mxQtUVAB76Py5g66Nun3c",
+    authDomain: "cs-4320.firebaseapp.com",
+    projectId: "cs-4320",
+    storageBucket: "cs-4320.appspot.com",
+    messagingSenderId: "1059831730619",
+    appId: "1:1059831730619:web:0678883c055819bf4a749e",
+    measurementId: "G-T0B6FMHQQD"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase();
+const dbRef = ref(database);
+
+async function readDatabase() {
+  var x = await get(child(dbRef, `reports/`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      //console.log(snapshot.val());
+      return snapshot.val();
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+  return x;
+}
+
+export function writeUserData(text) {
+  set(ref(database, 'test/' + test), {
+    test: text
+  });
+}
+
+writeUserData("bruhhhhhh");
