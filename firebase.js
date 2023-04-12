89a603c0c59a6ef063b8a9269777492fb2ab8b08
Phillip Lei <plgrx@umsystem.edu>
2023-04-12 14:34:12 -0500

    fixing write function

--- firebase.js
+++ firebase.js
@@ -3,15 +3,17 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase
 import { getDatabase, ref, child, get, set } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBhjZ1zYPBKw5mxQtUVAB76Py5g66Nun3c",
-    authDomain: "cs-4320.firebaseapp.com",
-    projectId: "cs-4320",
-    storageBucket: "cs-4320.appspot.com",
-    messagingSenderId: "1059831730619",
-    appId: "1:1059831730619:web:0678883c055819bf4a749e",
-    measurementId: "G-T0B6FMHQQD"
-  };
+  apiKey: "AIzaSyBhjZ1zYPBKw5mxQtUVAB76Py5g66Nun3c",
+  authDomain: "cs-4320.firebaseapp.com",
+  databaseURL: "https://cs-4320-default-rtdb.firebaseio.com",
+  projectId: "cs-4320",
+  storageBucket: "cs-4320.appspot.com",
+  messagingSenderId: "1059831730619",
+  appId: "1:1059831730619:web:0678883c055819bf4a749e",
+  measurementId: "G-T0B6FMHQQD"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -33,9 +35,9 @@ async function readDatabase() {
   return x;
 }
 
-export function writeUserData(text) {
-  set(ref(database, '/test/Test/firstCollection/firstDoc'), {
-    "test": text
+export function writeUserData(firstName, lastName) {
+  set(ref(database, 'users/'), {
+    firstName: lastName
   });
 }
 
